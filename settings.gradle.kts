77023dd666a4bb6afbd9795ef9a62e04d385ebee
j18801907041 <j18801907041@users.noreply.github.com>
2026-09-10 23:06:43 +0800

    fix: update kotlin.compose plugin version 2.3.12 -> 2.4.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
   plugins {
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.12" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
   }
   repositories {
     google {
